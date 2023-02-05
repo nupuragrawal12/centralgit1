e9e82367aa04f634a9ad23b3682ae24c06dd3dd9
noopur <noopurmor@gmail.com>
2023-02-05 11:32:22 +0530

    my first commit

---
+++ first_ec2.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIA2M64PJXW6Q2KBPNB"
+  secret_key = "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"
+}
+
+resource "aws_instance" "my-ec2"{
+ ami = "ami-01a4f99c4ac11b03c"
+ instance_type = "t2.micro"
+}
